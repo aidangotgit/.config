8378aee2708e81db25ab897f03d48bb5f6e46cb0
aidan <152641691+aidangotgit@users.noreply.github.com>
2024-02-14 22:02:19 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
 require("neotex.core")
 require("neotex.bootstrap")
+
+vim.o.background = "light"
+
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'jk', '<Esc>', { noremap = true, silent = true })
+
